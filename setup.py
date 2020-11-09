7574ce4b309528021c859f62eb9dd5919dd12c02
Phillip Taylor <phillipt@udel.edu>
2020-11-09 17:43:14 -0500

    Updated conversion factors

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
 setup(
-<<<<<<< HEAD
       name="mbuild_CLP",
       install_requires="mbuild",
       entry_points={
@@ -10,10 +9,3 @@ setup(
                     },
                     py_modules=["mbuild_CLP"],
                         )
-=======
-    name="mbuild_ona",
-    install_requires="mbuild",
-    entry_points={"mbuild.plugins": ["DNA = mbuild_ona.mbuild_ona:DNA"]},
-    py_modules=["mbuild_ona"],
-)
->>>>>>> 704e82ccbfc1088140a9cd18d7b6c468559b3963
